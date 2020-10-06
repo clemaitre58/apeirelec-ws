2e6072676bd7029b2e6ecebc8a569e2bcc402aaa
Cédric Lemaitre <c.lemaitre58@gmail.com>
2020-10-06 17:38:11 +0200

    fix email error typo + test ok

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -11,12 +11,12 @@ if(empty($_POST['name'])      ||
    }
 
    
-/* $name = strip_tags(htmlspecialchars($_POST['name']));
+$name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message'])); */
+$message = strip_tags(htmlspecialchars($_POST['message'])); 
 
-$name = "Cédric Lemaître";
+/*$name = "Cédric Lemaître";
 $email_address = "c.lemaitre@apeira-technologies.fr";
 $phone = "0672715584";
 $message = "test";
@@ -25,15 +25,16 @@ echo("test");
 echo($name);
 echo($email_address);
 echo($phone);
-echo($message);
+echo($message);*/
    
 // Create the email and send the message
-$to = 'contact@apeirlec.fr'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
+$to = 'contact@apeirelec.fr'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$Subject = "Website Contact Form:  $name";
 $email_body = "Nouveau message depuis le formulaire de contact.\n\n"."Voici les détails de ce message:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@apeirlec.fr\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-$CR_Mail = @mail($to,$email_subject,$email_body,$headers);
+$headers .= "Reply-To: $email_address";
+$CR_Mail = TRUE;
+$CR_Mail = @mail($to,$Subject,$email_body,$headers);
 if ($CR_Mail === FALSE) {
    echo " ### CR_Mail=$CR_Mail - Erreur envoi mail <br> \n";
 } else {
