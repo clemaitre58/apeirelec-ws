007405ede560614a78012074d3a83291e5474b65
Cédric Lemaitre <c.lemaitre58@gmail.com>
2020-10-06 16:29:41 +0200

    add print to debug

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -9,11 +9,17 @@ if(empty($_POST['name'])      ||
    echo "No arguments Provided!";
    return false;
    }
+
    
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
+
+print($name);
+print($email_address);
+print($phone);
+print($message);
    
 // Create the email and send the message
 $to = 'contact@apeirlec.fr'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
