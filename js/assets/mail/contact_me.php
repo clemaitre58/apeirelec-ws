e042132d1cf91f6c54fe3686040831e03ee222ef
LEMAITRE Cedric <c.lemaitre58@gmail.com>
2022-06-20 11:06:02 +0200

    Version clim

---
+++ js/assets/mail/contact_me.php
@@ -0,0 +1,32 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+print($name);
+print($email_address);
+print($phone);
+print($message);
+   
+// Create the email and send the message
+$to = 'contact@apeirlec.fr'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "Nouveau message depuis le formulaire de contact.\n\n"."Voici les détails de ce message:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@apeirlec.fr\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+return true;         
+?>
